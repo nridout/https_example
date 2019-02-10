336f9870e3401d087344c8537a97dc700fa92a49
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-09 20:43:35 -0500

    added response script

--- https_example.js
+++ https_example.js
@@ -3,13 +3,14 @@ var https = require('https');
 console.log('I did it!');
 
 var options = {
-  host: 'www.example.org',
+  host: 'www.google.ca',
   path: '/'
 };
 
 // called by https when the request is made.
-var callback = function () {
+var callback = function (response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
